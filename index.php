3eba025f292bc1e3c87e68a46e371f61de44d8e0
AnjaHH <anja2590@easv365.dk>
2023-05-22 11:40:08 +0200

    last changes

--- index.php
+++ index.php
@@ -82,29 +82,25 @@ $embed = get_field("video");
                 <h3><?php the_field("headline2") ?></h3>
             </div>
             <div class="gallery-carousel reveal">
+
             <div class="image-container">
+            
   <div class="image-wrapper">
+  <?php $loop = new WP_Query(array('post_type' => 'carousel', 'orderby' => 'post_id', 'order' => 'ASC')); ?>
+                        <?php while ($loop->have_posts()) : $loop->the_post(); ?>
     <div>
-      <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="">
+        <img src="<?php the_field("carousel-image") ?>" alt="">    
     </div>
-    <div>
-      <img src="https://images.unsplash.com/photo-1607522370275-f14206abe5d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="">
-    </div>
-    <div>
-      <img src="https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c2hvZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="">
-    </div>
-    <div>
-      <img src="https://images.unsplash.com/photo-1608231387042-66d1773070a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2hvZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="">
-    </div>
-    <div>
-      <img src="https://images.unsplash.com/photo-1605348532760-6753d2c43329?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2hvZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="">
-    </div>
-    
-  </div>
-</div>
+   
 
-            </div>
 
+            
+            <?php endwhile; ?>
+                        <?php wp_reset_postdata(); ?>
+                        </div>
+                        </div>
+
+        </div>
         </div>
 
         
--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 50;
+      var elementVisible = 30;
   
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
